1eec22686dfc3e1a28d345007157ba11f535d01a
Connor Kennedy <cjtkennedy@gmail.com>
2018-01-31 00:13:21 -0600

    Make a basic test middleware

---
+++ TheBot/Middleware/TestMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Noobot.Core.MessagingPipeline.Middleware;
+using Noobot.Core.MessagingPipeline.Middleware.ValidHandles;
+using Noobot.Core.MessagingPipeline.Request;
+using Noobot.Core.MessagingPipeline.Response;
+using System.Collections.Generic;
+
+namespace TheBot.Middleware
+{
+    public class TestMiddleware : MiddlewareBase
+    {
+        public TestMiddleware(IMiddleware next) : base(next)
+        {
+            HandlerMappings = new[]
+            {
+                new HandlerMapping
+                {
+                    ValidHandles = ExactMatchHandle.For("tttt"),
+                    EvaluatorFunc = TestHandler,
+                    Description = "This is the description for the test handler",
+                    VisibleInHelp = true
+                }
+            };
+        }
+
+        private IEnumerable<ResponseMessage> TestHandler(IncomingMessage message, IValidHandle matchedHandle)
+        {
+            yield return message.ReplyDirectlyToUser("Message received!");
+        }
+    }
+}
