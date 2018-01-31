9675adde9bccaa2d3fe549442dfbd0a08599f089
Connor Kennedy <cjtkennedy@gmail.com>
2018-01-31 00:48:29 -0600

    Add custom configuration for middleware injection

---
+++ TheBot/Configuration/NoobotConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Noobot.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Noobot.Toolbox.Middleware;
+using TheBot.Middleware;
+
+namespace TheBot.Configuration
+{
+    public class NoobotConfiguration : ConfigurationBase
+    {
+        public NoobotConfiguration()
+        {
+            //UseMiddleware<AdminMiddleware>();
+            UseMiddleware<JokeMiddleware>();
+            UseMiddleware<PingMiddleware>();
+            UseMiddleware<TestMiddleware>();
+        }
+    }
+}
