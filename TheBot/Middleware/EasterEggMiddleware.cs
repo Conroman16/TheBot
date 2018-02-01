ae22944a2d0c8df11000b87b96992fc55e110670
Connor Kennedy <cjtkennedy@gmail.com>
2018-01-31 21:43:12 -0600

    Add easter egg middleware

---
+++ TheBot/Middleware/EasterEggMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Noobot.Core.MessagingPipeline.Middleware;
+using Noobot.Core.MessagingPipeline.Middleware.ValidHandles;
+using Noobot.Core.MessagingPipeline.Request;
+using Noobot.Core.MessagingPipeline.Response;
+using System.Collections.Generic;
+
+namespace TheBot.Middleware
+{
+    public class EasterEggMiddleware : MiddlewareBase
+    {
+        public EasterEggMiddleware(IMiddleware next) : base(next)
+        {
+            HandlerMappings = new[]
+            {
+                new HandlerMapping
+                {
+                    ValidHandles = RegexHandle.For("open (.*) door"),
+                    EvaluatorFunc = PodBayDoorHandler,
+                    Description = "Opens the pod bay doors",
+                    VisibleInHelp = false
+                }
+            };
+        }
+
+        private IEnumerable<ResponseMessage> PodBayDoorHandler(IncomingMessage message, IValidHandle matchedHandle)
+        {
+            yield return message.ReplyToChannel($"I'm afraid I can't let you do that {message.Username}");
+        }
+    }
+}
